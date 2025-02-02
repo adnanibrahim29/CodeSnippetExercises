2f0eaecc27237376d14163cffbd35d80ae50fc27
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-02-02 20:15:40 +0000

    lambdaSorting

---
+++ Java/lambdaSorting.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class lambdaSorting {
+
+    /**
+     * Current code prints out the Array in alphabetical order
+     * 
+     * Task - Modify the program to print the array in reverse order
+     * @param args
+     */
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Charlie", "Bob", "David");
+
+        /**
+         * Sorting using a lambda expression
+         * Change: swap a and b 
+         * names.sort((a, b) -> a.compareTo(b)); // a is smaller than b
+         */
+        names.sort((b, a) -> a.compareTo(b)); // b is smaller than a
+
+        // Print sorted names
+        names.forEach(name -> System.out.print(name + " "));
+    }
+}
